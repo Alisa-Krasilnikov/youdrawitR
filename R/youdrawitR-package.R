d5f6289c68f61e2f1d6932d1eb4ee5b7c0ae0745
Alisa-Krasilnikov <akrasiln@calpoly.edu>
2026-04-15 15:06:31 -0700

    Save current local work after repo repair

---
+++ R/youdrawitR-package.R
@@ -0,0 +1,16 @@
+#' @title youdrawitR: A Graphical Testing Tool Designed for Use with ggplot2
+#'
+#' @description
+#' \figure{logo.png}{options: style='float: right; height: 200px'}
+#' The youdrawitR package provides tools for creating interactive “You Draw It”
+#' visualizations using *ggplot2* and *D3*. Users can convert static plots into
+#' interactive graphics, draw trends, and generate data through direct interaction.
+#' The package supports both guided drawing through \code{drawit()} and flexible,
+#' free-form drawing through \code{sketchit()}.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
